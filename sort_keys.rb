033d53c91ea160684b5592a0875f00e5acdeb7c9
shadish <shadtisch@gmail.com>
2018-08-26 00:07:53 -0600

    improve key sorting

--- sort_keys.rb
+++ sort_keys.rb
@@ -1,5 +1,5 @@
 
 def sort_keys(c)
-	ordinal = ['chapter', 'name', 'ancestry', 'age', 'background', 'class', 'class_details', 'attributes']
+	ordinal = ['chapter', 'name', 'ancestry', 'age', 'background', 'skills', 'ac','tac', 'class', 'class_details', 'attributes']
 	c.sort_by { |k,v| (ordinal.index(k) || 999) }.to_h
 end
